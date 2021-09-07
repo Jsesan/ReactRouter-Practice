6d91af3356561877fc334d54b160d336074d879b
Jsesan <jsesan00@gmail.com>
2021-09-07 09:47:53 -0300

    404 page not found added

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Juan", text: "lalalala" },
+  { id: "q2", author: "Juan", text: "lolololo" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,24 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlatedQuote from "../components/quotes/HighlightedQuote";
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Juan", text: "lalalala" },
+  { id: "q2", author: "Juan", text: "lolololo" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>Not Found</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail</h1>
-      <p>{params.quoteId}</p>
+      <HighlatedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/` + params.quoteId + `/comments`}>
         <Comments />
       </Route>
